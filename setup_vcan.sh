a9baae8c1a8112511fb2f369ac4dde33c2b9aa93
spenghui <178119008@qq.com>
2017-03-04 15:53:12 +0800

    modified setup_vcan.sh
    to make setup_vcan.sh can accept params.

--- setup_vcan.sh
+++ setup_vcan.sh
@@ -1,4 +1,14 @@
 sudo modprobe can
 sudo modprobe vcan
-sudo ip link add dev vcan0 type vcan
-sudo ip link set up vcan0
+
+for arg in "$@"
+   do
+   sudo ip link add dev $arg type vcan
+   sudo ip link set up $arg
+done
+
+
+#sudo modprobe can
+#sudo modprobe vcan
+#sudo ip link add dev vcan0 type vcan
+#sudo ip link set up vcan0
